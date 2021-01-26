23b90bc0c9990816d0b2ba2f5356b697b076b1cc
GeorgeSabu <sabu.george@elucidata.io>
2021-01-26 13:47:08 +0530

    Update shall_i_run.sh

--- shall_i_run.sh
+++ shall_i_run.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-if [[ $(date +%u) -gt 5 ]]
-then 
-  echo "Going to check if the job can be scheduled"
-  exit 0
-else 
-  echo "Normal run"   
-    currenttime=$(date +%H:%M)
-    echo $currenttime
-    if [[ "$currenttime" > "17:00" ]] || [[ "$currenttime" < "12:00" ]]; then
-      echo "Going to check if the job can be scheduled"
-      exit 0
-    else
-      echo "It not night to run"
-      exit 0
-    fi
-fi
+exit 0
+#if [[ $(date +%u) -gt 5 ]]
+#then 
+#  echo "Going to check if the job can be scheduled"
+#  exit 0
+#else 
+#  echo "Normal run"   
+#    currenttime=$(date +%H:%M)
+#    echo $currenttime
+#    if [[ "$currenttime" > "17:00" ]] || [[ "$currenttime" < "12:00" ]]; then
+#      echo "Going to check if the job can be scheduled"
+#      exit 0
+#    else
+#      echo "Going to check if the job can be scheduled"
+#      exit 0
+#    fi
+#fi
