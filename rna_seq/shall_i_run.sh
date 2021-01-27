d366b80b8ec878e1c87aba97e63cab605ee10743
GeorgeSabu <sabu.george@elucidata.io>
2021-01-27 12:01:04 +0530

    Add files via upload

---
+++ rna_seq/shall_i_run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+exit 0
+#if [[ $(date +%u) -gt 5 ]]
+#then 
+#  echo "Going to check if the job can be scheduled"
+#  exit 0
+#else 
+#  echo "Normal run"   
+#    currenttime=$(date +%H:%M)
+#    echo $currenttime
+#    if [[ "$currenttime" > "17:00" ]] || [[ "$currenttime" < "12:00" ]]; then
+#      echo "Going to check if the job can be scheduled"
+#      exit 0
+#    else
+#      echo "Going to check if the job can be scheduled"
+#      exit 0
+#    fi
+#fi
