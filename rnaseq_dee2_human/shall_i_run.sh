fb951fe23a72f65582e91b166303ef7d057257ad
GeorgeSabu <sabu.george@elucidata.io>
2021-01-27 22:27:08 +0530

    Update shall_i_run.sh

--- rnaseq_dee2_human/shall_i_run.sh
+++ rnaseq_dee2_human/shall_i_run.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-exit 0
+K=$(polly jobs status --workspace-id 6583 > data.log; cat data.log | grep "RUNNING" | wc -l)
+MAX=5
+if [ "$K" -gt "$MAX" ]
+then
+        exit 1
+else
+        exit 0
+fi
