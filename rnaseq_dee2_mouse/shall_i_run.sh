6cd0168fe41501b436f7f97c9fdaae20b87b8da0
Harshit Manek <harshit.manek@elucidata.io>
2021-01-29 10:29:56 +0530

    adding a folder for mouse

---
+++ rnaseq_dee2_mouse/shall_i_run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+K=$(polly jobs status --workspace-id $WORKSPACE_ID -y > data.log; cat data.log | grep "RUNNING" | wc -l)
+MAX=10
+if [ "$K" -gt "$MAX" ]
+then
+        exit 1
+else
+        exit 0
+fi
